13c56cbe30e5158e84f8a7491057c6752da280af
AW Coder <71630490+abdulahhab121786@users.noreply.github.com>
2022-07-20 22:53:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let str='Welcome to code with AW';
+console.log(str);
+console.log("mentioned string's character number 5 is " + str.charAt(5));
+let str2=str.concat(' and Abdul wahhab is an exper developer.');
+console.log(str2)
+
+console.log(str.slice(0,7))
+console.log(str.lastIndexOf(2));
